73b1060d8a20e8e7bad6baae5d07a72344e4a4c6
xcnoris <augustobriel12@gmail.com>
2023-10-02 23:23:14 -0300

    Começos  do  estudo sobre objetos em C#

---
+++ ScreenSound/ScreenSound/Program.cs
@@ -0,0 +1,18 @@
+﻿Musica  musica1 = new Musica();
+musica1.nome = "Uma Arlinda Mulher";
+musica1.artista = "Mamonas Assasinas";
+musica1.duração = 180;
+musica1.disponivel = true;
+
+Musica musica2 = new Musica();
+musica2.nome = "Tempo Perdido";
+musica2.artista = "Legião Urbana";
+musica2.duração = 300;
+musica2.disponivel = false;
+
+musica1.ExibirFicaTecnica();
+musica2.ExibirFicaTecnica();
+
+musica1.ExibirNomeEArtista();
+
+
