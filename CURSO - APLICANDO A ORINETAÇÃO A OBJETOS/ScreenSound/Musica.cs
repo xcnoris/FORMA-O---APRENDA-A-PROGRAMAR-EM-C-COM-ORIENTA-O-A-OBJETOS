532d8a726363fadf8daac265ccf827aa444c16c5
Augusto Nascimento <134967419+xcnoris@users.noreply.github.com>
2023-12-07 08:26:58 -0300

    CURSO - APLICANDO A ORIENTAÇÃO A OBJETOS
    Nesse curso vemos: 
    Crie e manipule classes e objetos em C#
    Utilize propriedades e métodos para gerenciar atributos
    Desenvolva habilidades em programação orientada a objetos
    Trabalhe com listas e lambdas em C#
    Gerencie relacionamentos entre classes no Visual Studio
    Aplique construtores e parâmetros em C#

---
+++ CURSO - APLICANDO A ORINETAÇÃO A OBJETOS/ScreenSound/Musica.cs
@@ -0,0 +1,27 @@
+﻿class Musica
+{
+    public  string nome;
+    public string artista;
+    public int duração;
+    public bool disponivel;
+
+    public void ExibirFichaTecnica()
+    {
+        Console.WriteLine($"Nome: {nome}");
+        Console.WriteLine($"Artista: {artista}");
+        Console.WriteLine($"Duração: {duração}");
+        if (disponivel)
+        {
+            Console.WriteLine("Disponibilidade: Disponivel no plano");
+        } else
+        {
+            Console.WriteLine("Disponibilidade: Adquira o plano Plus+");
+        };
+    }
+
+    public void ExibirNomeEArtista()
+        {
+            Console.WriteLine($"Nome/Artista: {nome} - {artista}");
+        }
+
+    }
