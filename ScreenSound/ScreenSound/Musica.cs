d0954efbd9e2d3880c8e97156c7a7e096c5a204c
Augusto Nascimento <augustobriel12@gmail.com>
2023-12-12 14:55:44 -0300

    Mostrando a propriedade DescricaoResumida

--- ScreenSound/ScreenSound/Musica.cs
+++ ScreenSound/ScreenSound/Musica.cs
@@ -1,27 +1,36 @@
 ﻿class Musica
 {
-    public  string nome;
-    public string artista;
-    public int duração;
-    public bool disponivel;
+    public string Nome { get; set; }
+    public string Artista { get; set; }
+    public int Duracao { get; set; }
+    public bool Disponivel { get; set; }
+
+    public string DescricaoResumida
+    {
+        get
+        {
+            return $"A música {Nome} pertence à banda {Artista}";
+        }
+    }
 
     public void ExibirFichaTecnica()
     {
-        Console.WriteLine($"Nome: {nome}");
-        Console.WriteLine($"Artista: {artista}");
-        Console.WriteLine($"Duração: {duração}");
-        if (disponivel)
+        Console.WriteLine($"Nome: {Nome}");
+        Console.WriteLine($"Artista: {Artista}");
+        Console.WriteLine($"Duração: {Duracao}");
+        if (Disponivel)
         {
             Console.WriteLine("Disponibilidade: Disponivel no plano");
-        } else
+        }
+        else
         {
             Console.WriteLine("Disponibilidade: Adquira o plano Plus+");
         };
     }
 
     public void ExibirNomeEArtista()
-        {
-            Console.WriteLine($"Nome/Artista: {nome} - {artista}");
-        }
-
+    {
+        Console.WriteLine($"Nome/Artista: {Nome} - {Artista}");
     }
+
+}
--- ScreenSound/ScreenSound/Program.cs
+++ ScreenSound/ScreenSound/Program.cs
@@ -1,18 +1,17 @@
-﻿Musica  musica1 = new Musica();
-musica1.nome = "Uma Arlinda Mulher";
-musica1.artista = "Mamonas Assasinas";
-musica1.duração = 180;
-musica1.disponivel = true;
+﻿Musica musica1 = new Musica();
+musica1.Nome = "Uma Arlinda Mulher";
+musica1.Artista = "Mamonas Assasinas";
+musica1.Duracao = 180;
+musica1.Disponivel = true;
+Console.WriteLine(musica1.DescricaoResumida);
 
 Musica musica2 = new Musica();
-musica2.nome = "Tempo Perdido";
-musica2.artista = "Legião Urbana";
-musica2.duração = 300;
-musica2.disponivel = false;
+musica2.Nome = "Tempo Perdido";
+musica2.Artista = "Legião Urbana";
+musica2.Duracao = 300;
+musica2.Disponivel = false;
 
 musica1.ExibirFichaTecnica();
 musica2.ExibirFichaTecnica();
-
-musica1.ExibirNomeEArtista();
 
 
